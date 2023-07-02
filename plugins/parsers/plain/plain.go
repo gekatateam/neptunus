7219dfb1f3cda32a9d9955418c170555c352e856
M0rdecay <relikts2158@gmail.com>
2023-07-03 01:50:09 +0300

    plain parser

---
+++ plugins/parsers/plain/plain.go
@@ -0,0 +1,57 @@
+package plain
+
+import (
+	"time"
+
+	"github.com/gekatateam/neptunus/core"
+	"github.com/gekatateam/neptunus/logger"
+	"github.com/gekatateam/neptunus/metrics"
+	"github.com/gekatateam/neptunus/pkg/mapstructure"
+	"github.com/gekatateam/neptunus/plugins"
+)
+
+type Plain struct {
+	alias string
+	pipe  string
+	Field string `mapstructure:"field"`
+	log   logger.Logger
+}
+
+func (p *Plain) Init(config map[string]any, alias, pipeline string, log logger.Logger) error {
+	if err := mapstructure.Decode(config, p); err != nil {
+		return err
+	}
+
+	p.alias = alias
+	p.pipe = pipeline
+	p.log = log
+
+	return nil
+}
+
+func (p *Plain) Alias() string {
+	return p.alias
+}
+
+func (p *Plain) Close() error {
+	return nil
+}
+
+func (p *Plain) Parse(data []byte, routingKey string) ([]*core.Event, error) {
+	now := time.Now()
+
+	event := core.NewEventWithData(routingKey, core.Map{
+		p.Field: string(data),
+	})
+	metrics.ObserveParserSummary("plain", p.alias, p.pipe, metrics.EventAccepted, time.Since(now))
+
+	return []*core.Event{event}, nil
+}
+
+func init() {
+	plugins.AddParser("plain", func() core.Parser {
+		return &Plain{
+			Field: "event",
+		}
+	})
+}
