5c4621e3b6057a689a76db8e636a1711ff762414
M0rdecay <relikts2158@gmail.com>
2023-07-02 20:31:53 +0300

    regex processor added

---
+++ plugins/processors/regex/regex.go
@@ -0,0 +1,142 @@
+package regex
+
+import (
+	"fmt"
+	"regexp"
+	"time"
+
+	"github.com/gekatateam/neptunus/core"
+	"github.com/gekatateam/neptunus/logger"
+	"github.com/gekatateam/neptunus/metrics"
+	"github.com/gekatateam/neptunus/pkg/mapstructure"
+	"github.com/gekatateam/neptunus/plugins"
+)
+
+type Regex struct {
+	alias  string
+	pipe   string
+	Labels map[string]string `mapstructure:"labels"`
+	Fields map[string]string `mapstructure:"fields"`
+
+	in  <-chan *core.Event
+	out chan<- *core.Event
+	log logger.Logger
+
+	fields map[string]*regexp.Regexp
+	labels map[string]*regexp.Regexp
+}
+
+func (p *Regex) Init(config map[string]any, alias, pipeline string, log logger.Logger) error {
+	if err := mapstructure.Decode(config, p); err != nil {
+		return err
+	}
+
+	p.alias = alias
+	p.pipe = pipeline
+	p.log = log
+	p.labels = make(map[string]*regexp.Regexp)
+	p.fields = make(map[string]*regexp.Regexp)
+
+	for k, v := range p.Labels {
+		regex, err := regexp.Compile(v)
+		if err != nil {
+			return fmt.Errorf("label %v regex compilation failed: %v", k, err)
+		}
+		p.labels[k] = regex
+	}
+
+	for k, v := range p.Fields {
+		regex, err := regexp.Compile(v)
+		if err != nil {
+			return fmt.Errorf("field %v regex compilation failed: %v", k, err)
+		}
+		p.fields[k] = regex
+	}
+
+	return nil
+}
+
+func (p *Regex) Prepare(
+	in <-chan *core.Event,
+	out chan<- *core.Event,
+) {
+	p.in = in
+	p.out = out
+}
+
+func (p *Regex) Close() error {
+	return nil
+}
+
+func (p *Regex) Alias() string {
+	return p.alias
+}
+
+func (p *Regex) Run() {
+	for e := range p.in {
+		now := time.Now()
+		if err := p.match(e); err != nil {
+			e.StackError(err)
+			metrics.ObserveProcessorSummary("regex", p.alias, p.pipe, metrics.EventFailed, time.Since(now))
+			goto SEND_OUT
+		}
+		metrics.ObserveProcessorSummary("regex", p.alias, p.pipe, metrics.EventAccepted, time.Since(now))
+	SEND_OUT:
+		p.out <- e
+	}
+}
+
+func (p *Regex) match(e *core.Event) error {
+	for name, rex := range p.labels {
+		label, found := e.GetLabel(name)
+		if !found {
+			continue
+		}
+
+		match := rex.FindStringSubmatch(label)
+		if len(match) == 0 {
+			continue
+		}
+
+		for i, name := range rex.SubexpNames() {
+			if i != 0 && name != "" {
+				e.AddLabel(name, match[i])
+			}
+		}
+	}
+
+	for name, rex := range p.fields {
+		rawField, err := e.GetField(name)
+		if err != nil {
+			continue
+		}
+
+		field, ok := rawField.(string)
+		if !ok {
+			continue
+		}
+
+		match := rex.FindStringSubmatch(field)
+		if len(match) == 0 {
+			continue
+		}
+
+		dataCopy := e.Data.Clone()
+		for i, name := range rex.SubexpNames() {
+			if i != 0 && name != "" {
+				if err := dataCopy.SetValue(name, match[i]); err != nil {
+					return fmt.Errorf("field %v set error: %v", name, err)
+				}
+			}
+		}
+		e.Data = dataCopy
+	}
+
+	return nil
+}
+
+func init() {
+	plugins.AddProcessor("regex", func() core.Processor {
+		return &Regex{}
+	})
+}
