2fd49cf2ebaa2f9d55b2e64b306db334205cebea
M0rdecay <relikts2158@gmail.com>
2024-06-13 23:57:55 +0300

    output exec WIP

--- plugins/outputs/all.go
+++ plugins/outputs/all.go
@@ -2,6 +2,7 @@ package outputs
 
 import (
 	_ "github.com/gekatateam/neptunus/plugins/outputs/elasticsearch"
+	_ "github.com/gekatateam/neptunus/plugins/outputs/exec"
 	_ "github.com/gekatateam/neptunus/plugins/outputs/grpc"
 	_ "github.com/gekatateam/neptunus/plugins/outputs/kafka"
 	_ "github.com/gekatateam/neptunus/plugins/outputs/log"
