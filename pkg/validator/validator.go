3b42561285a4c77f62c2f27b3f5e371fcc514792
M0rdecay <relikts2158@gmail.com>
2024-06-23 01:32:57 +0300

    http input WIP

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -1,6 +1,8 @@
 package validator
 
 import (
+	"regexp"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/go-playground/validator/v10/non-standard/validators"
 )
@@ -11,6 +13,23 @@ var v *validator.Validate = func() *validator.Validate {
 	return v
 }()
 
+// https://github.com/go-playground/validator/blob/master/regexes.go#L69
+var splitParamsRegexp = regexp.MustCompile(`'[^']*'|\S+`)
+
+// not used right now, but still may be usefull in feature
 func Validate(s any) error {
 	return v.Struct(s)
 }
+
+// func ManyOf(fl validator.FieldLevel) bool {
+// 	if len(fl.Param()) == 0 {
+// 		return false
+// 	}
+
+// 	vals := splitParamsRegexp.FindAllString(fl.Param(), -1)
+// 	for i := 0; i < len(vals); i++ {
+// 		vals[i] = strings.Replace(vals[i], "'", "", -1)
+// 	}
+
+// 	field := fl.Field()
+// }
