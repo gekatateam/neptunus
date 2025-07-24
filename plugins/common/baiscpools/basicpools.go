29e54606414ee5f8ff47d7dfc9a23c6d61c34f4a
M0rdecay <relikts2158@gmail.com>
2025-07-24 23:28:46 +0300

    use generic wrapper over sync.Pool

--- plugins/common/baiscpools/basicpools.go
+++ plugins/common/baiscpools/basicpools.go
@@ -13,14 +13,7 @@ var BytesBuffer = Pool[*bytes.Buffer]{
 	},
 }
 
-type Pool[T any] struct {
-	p *sync.Pool
-}
+type Pool[T any] struct{ p *sync.Pool }
 
-func (p Pool[T]) Get() T {
-	return p.p.Get().(T)
-}
-
-func (p Pool[T]) Put(x T) {
-	p.p.Put(x)
-}
+func (p Pool[T]) Get() T  { return p.p.Get().(T) }
+func (p Pool[T]) Put(x T) { p.p.Put(x) }
