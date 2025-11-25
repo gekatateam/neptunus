162daeee76d23ae575f76ebf0044c35690379741
M0rdecay <relikts2158@gmail.com>
2025-11-25 19:53:35 +0300

    starlark error as pkg

---
+++ pkg/starlarkerror/error.go
@@ -0,0 +1,61 @@
+package starlarkerror
+
+import (
+	"errors"
+
+	"go.starlark.net/starlark"
+	"go.starlark.net/starlarkstruct"
+)
+
+var Module = &starlarkstruct.Module{
+	Name: "error",
+	Members: starlark.StringDict{
+		"new":    starlark.NewBuiltin("new", New),
+		"handle": starlark.NewBuiltin("handle", Handle),
+	},
+}
+
+var _ starlark.Value = Error("")
+
+type Error string
+
+func (e Error) String() string {
+	return string(e)
+}
+
+func (e Error) Type() string {
+	return "error.error"
+}
+
+func (e Error) Freeze() {}
+
+func (e Error) Truth() starlark.Bool {
+	return len(e) > 0
+}
+
+func (e Error) Hash() (uint32, error) {
+	return 0, errors.New("error.error is not hashable")
+}
+
+func New(_ *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var msg string
+	if err := starlark.UnpackPositionalArgs(b.Name(), args, kwargs, 1, &msg); err != nil {
+		return starlark.None, err
+	}
+
+	return Error(msg), nil
+}
+
+// handle accepts Callable and wrap occurred err into Error
+func Handle(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var fn starlark.Callable
+	if err := starlark.UnpackPositionalArgs("handle", args, kwargs, 1, &fn); err != nil {
+		return starlark.None, err
+	}
+
+	result, err := starlark.Call(thread, fn, nil, nil)
+	if err != nil {
+		return Error(err.Error()), nil
+	}
+	return result, nil
+}
