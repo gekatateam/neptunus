fe1878e14a6e894e345588835ff83c3243ef0565
M0rdecay <relikts2158@gmail.com>
2025-11-29 20:24:32 +0300

    basic queries

--- pipeline/storage/postgresql.go
+++ pipeline/storage/postgresql.go
@@ -27,10 +27,10 @@ import (
 const (
 	migrate = `
 CREATE TABLE IF NOT EXISTS pipelines (
-	created_at    TIMESTAMP WITH TIME ZONE
+	created_at    TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 	, updated_at  TIMESTAMP WITH TIME ZONE
 	, deleted_at  TIMESTAMP WITH TIME ZONE
-	, id          TEXT
+	, id          TEXT CONSTRAINT unique_ids_only UNIQUE
 	, lines       INTEGER
 	, run         BOOLEAN
 	, buffer      INTEGER
@@ -42,6 +42,46 @@ CREATE TABLE IF NOT EXISTS pipelines (
 	, processors  JSON
 	, outputs     JSON
 );`
+
+	add = `
+INSERT INTO pipelines 
+(id, lines, run, buffer, consistency, log_level, vars, keykeepers, inputs, processors, outputs)
+VALUES
+($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`
+
+	add_check = `
+SELECT id, deleted_at FROM pipelines
+WHERE id = $1;`
+
+	delete = `
+DELETE FROM pipelines
+WHERE id = $1;`
+
+	update = `
+UPDATE pipelines 
+SET
+	updated_at = NOW()
+	, lines       = $2
+	, run         = $3
+	, buffer      = $4
+	, consistency = $5
+	, log_level   = $6
+	, vars        = $7
+	, keykeepers  = $8
+	, inputs      = $9
+	, processors  = $10
+	, outputs     = $11
+WHERE id = $1;`
+
+	get = `
+SELECT lines, run, buffer, consistency, log_level, vars, keykeepers, inputs, processors, outputs
+FROM pipelines
+WHERE id = $1 and deleted_at IS NULL;`
+
+	list = `
+SELECT id, lines, run, buffer, consistency, log_level, vars, keykeepers, inputs, processors, outputs
+FROM pipelines
+WHERE deleted_at IS NULL;`
 )
 
 type postgresql struct {
@@ -50,3 +90,10 @@ type postgresql struct {
 func PostgreSQL(cfg config.PostgresqlStorage) (*postgresql, error) {
 	panic(errors.ErrUnsupported)
 }
+
+func (s *postgresql) List() ([]*config.Pipeline, error)
+func (s *postgresql) Get(id string) (*config.Pipeline, error)
+func (s *postgresql) Add(pipe *config.Pipeline) error
+func (s *postgresql) Update(pipe *config.Pipeline) error
+func (s *postgresql) Delete(id string) error
+func (s *postgresql) Close() error
