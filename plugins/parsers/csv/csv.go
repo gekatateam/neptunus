5f897b369ba58001fdd45b24b2bcb8ec353f874f
M0rdecay <relikts2158@gmail.com>
2026-02-19 22:24:54 +0300

    csv parser - WIP, vertical mode

--- plugins/parsers/csv/csv.go
+++ plugins/parsers/csv/csv.go
@@ -27,11 +27,14 @@ type Csv struct {
 	LazyQuotes       bool   `mapstructure:"lazy_quotes"`
 	Comma            rune   `mapstructure:"comma"`
 	Comment          rune   `mapstructure:"comment"`
+
+	parseFunc func(data []byte, routingKey string) ([]*core.Event, error)
 }
 
 func (p *Csv) Init() error {
 	switch p.Mode {
 	case ModeHorizontal:
+		p.parseFunc = p.parseHorizontal
 	case ModeVertical:
 		if len(p.KeyColumn) == 0 {
 			return errors.New("key column required")
@@ -40,6 +43,8 @@ func (p *Csv) Init() error {
 		if !p.HasHeader {
 			return errors.New("header required for vertical mode")
 		}
+
+		p.parseFunc = p.parseVertical
 	default:
 		return fmt.Errorf("unknown mode: %v", p.Mode)
 	}
@@ -54,19 +59,7 @@ func (p *Csv) Close() error {
 func (p *Csv) Parse(data []byte, routingKey string) ([]*core.Event, error) {
 	now := time.Now()
 
-	var (
-		events []*core.Event
-		err    error
-	)
-
-	switch p.Mode {
-	case ModeHorizontal:
-		events, err = p.parseHorizontal(data, routingKey)
-	// case ModeVertical:
-	// 	events, err = p.parseVertical(data, routingKey)
-	default:
-		panic(fmt.Errorf("totally unexpected - how you start it with this mode? %v", p.Mode))
-	}
+	events, err := p.parseFunc(data, routingKey)
 
 	if err != nil {
 		p.Observe(metrics.EventFailed, time.Since(now))
@@ -79,6 +72,52 @@ func (p *Csv) Parse(data []byte, routingKey string) ([]*core.Event, error) {
 	}
 
 	return events, nil
+}
+
+func (p *Csv) parseVertical(data []byte, routingKey string) ([]*core.Event, error) {
+	reader := csv.NewReader(bytes.NewReader(data))
+	reader.Comma = p.Comma
+	reader.Comment = p.Comment
+	reader.LazyQuotes = p.LazyQuotes
+	reader.ReuseRecord = true
+
+	headerRaw, err := reader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	header := slices.Clone(headerRaw)
+	keyIndex := slices.Index(header, p.KeyColumn)
+	if keyIndex == -1 {
+		return nil, fmt.Errorf("key column %v not found in header", p.KeyColumn)
+	}
+
+	body := map[string]any{}
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		key := ""
+		row := make(map[string]any, len(record)-1)
+		for i, v := range record {
+			if i == keyIndex {
+				key = v
+				continue
+			}
+
+			row[header[i]] = v
+		}
+
+		body[key] = row
+	}
+
+	return []*core.Event{core.NewEventWithData(routingKey, body)}, nil
 }
 
 func (p *Csv) parseHorizontal(data []byte, routingKey string) ([]*core.Event, error) {
